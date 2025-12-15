c00055a2f22c7dda1713931822bcdd5a00c80350
Peanutbuster01 <folinaxel@gmail.com>
2025-12-15 10:51:23 +0100

    Tinkered with the display grid

--- src/views/LobbyView.vue
+++ src/views/LobbyView.vue
@@ -1,16 +1,19 @@
 <template>
+<head>
 <title>Lobby - {{uiLabels.siteName}}</title>
+</head>
 <header>
     <div class="logo">
       {{ uiLabels.siteName }}
     </div>
-    </header>
+</header>
+<main>
 <div class = 'ruleDiv'>
-
+ Rules here
 </div>
 <div class = 'lobbyDiv'>
-
-</div>
+ Lobbystuff here
+</div></main>
 </template>
 <script>
     import io from 'socket.io-client';
@@ -51,5 +54,22 @@ header {
     text-align: center;
     margin: 3%;
     font-size: 1.5rem;
+}
+main {
+    display: grid;
+    grid-template-columns: 30%, 30%, 30%;
+    padding: 5%;
+}
+.lobbyDiv {
+    background-color: blue;
+    grid-column: 1 /span 2;
+    width: 100%;
+    height: auto;
+}
+.ruleDiv{
+    background-color: red;
+    grid-column: 3;
+    width: 100%;
+    height: auto;
 }
 </style>
